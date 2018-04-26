3ce35e919992e4f63f5fb561d852e3ed98b18432
dorotaszostak <dorota.szostak@gmail.com>
2018-04-26 23:23:21 +0200

    main page html and scss

---
+++ scripts/cars-slider.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $('.cars-slider').slick({
+    slidesToShow: 2,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: false,
+      arrows: true,
+  });
+});
